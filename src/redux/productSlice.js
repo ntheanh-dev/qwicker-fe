43e51eb2424f1c5ff3a7c3706d7e4d3e5b7aac79
Theanhmgt <theanhmgt66@gmail.com>
2024-01-30 21:26:24 +0700

    refactor:(redux): clear productSlice

--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -1,35 +1,25 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createSelector } from "@reduxjs/toolkit";
 
 const INIT_PRODUCT = {
     category: null,
-    quantity: 0,
+    quantity: 1,
+    mass: null,
     image: null,
 }
 
 const productSlice = createSlice({
-    name: 'orderdetail',
+    name: 'productSlice',
     initialState: {
         product: INIT_PRODUCT,
-        isFulFill: false,
         status: 'idle'
     },
     reducers: {
-        addCategory: (state, action) => {
-            state.product.category = action.payload
-            state.isFulFill = isFulFill(state.isFulFill)
-        },
-        addQuantity: (state, action) => {
-            state.product.quantity = action.payload
-            state.isFulFill = isFulFill(state.isFulFill)
-        },
-        addImage: (state, action) => {
-            state.product.image = action.payload
-            state.isFulFill = isFulFill(state.isFulFill)
-        },
-        removeProduct: (state, action) => {
+        removeProductData: (state, action) => {
             state.product = INIT_PRODUCT
-            state.isFulFill = false
         },
+        setProduct: (state, action) => {
+            state.product = action.payload
+        }
     }
 })
 const isFulFill = (obj) => {
@@ -40,7 +30,13 @@ const isFulFill = (obj) => {
     })
     return true
 }
-export const getIsFulFill = state => state.productSlice.isFulFill
 export const getProduct = state => state.productSlice.product
-export const { addCategory, addQuantity, addImage, removeProduct } = productSlice.actions
+export const isProductFulFill = createSelector(
+    getProduct,
+    (product) => {
+        const { category, mass, quantity, image } = product
+        return category && mass && quantity && image
+    }
+)
+export const { removeProductData, setProduct } = productSlice.actions
 export default productSlice.reducer
--- src/screens/orderprocess/OrderDetail.js
+++ src/screens/orderprocess/OrderDetail.js
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, ScrollView, TextInput, FlatList, TouchableHighlight } from 'react-native'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
 import { Feather, AntDesign, Ionicons, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 import CheckBox from 'react-native-check-box'
 import * as ImagePicker from 'expo-image-picker';
@@ -7,27 +7,30 @@ import Dialog from "react-native-dialog";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProductCategories, getCategories } from '../../redux/appSlice';
 import { unwrapResult } from '@reduxjs/toolkit';
-import { addCategory, addImage, addQuantity } from '../../redux/productSlice';
+import { getProduct, removeProductData, setProduct } from '../../redux/productSlice';
 
 const massType = [{ id: 1, name: 'Nhẹ hơn 10 kg' }, { id: 2, name: '10 kg đến 30 kg' }, { id: 3, name: '30 kg đến 50 kg' }]
 
 const OrderDetail = ({ navigation }) => {
     const dispatch = useDispatch()
     const initCategories = useSelector(getCategories)
+    const initProduct = useSelector(getProduct)
     const [categories, setCategories] = useState(initCategories)
 
-    const [selectedCategory, setSelectedCategory] = useState(null)
-    const [checkedMass, setCheckedMass] = useState(null)
-    const [quantity, setQuantity] = useState(1)
-    const [image, setImage] = useState(null)
     const inputRef = useRef()
     const [visible, setVisible] = useState(false);
+    const [productDetail, updateProductDetail] = useReducer((prve, next) => ({
+        ...prve, ...next
+    }), {
+        selectedCategory: initProduct.category,
+        checkedMass: initProduct.mass,
+        quantity: initProduct.quantity,
+        image: initProduct.image
+    })
 
     const handleDelete = () => {
         setVisible(false);
-        setSelectedCategory(null)
-        setCheckedMass(null)
-        setQuantity(1)
+        dispatch(removeProductData)
         navigation.goBack()
     };
 
@@ -43,11 +46,12 @@ const OrderDetail = ({ navigation }) => {
                 base64: true,
             });
             if (!result.canceled) {
-                setImage(result.assets[0].uri)
+                updateProductDetail({ image: result.assets[0].uri })
             }
         }
     }
     const isFullField = () => {
+        const { selectedCategory, checkedMass, quantity, image } = productDetail
         return selectedCategory && checkedMass && image && Number(quantity) > 0
     }
     const handleBack = () => {
@@ -81,9 +85,13 @@ const OrderDetail = ({ navigation }) => {
 
     const handleSave = () => {
         if (isFullField()) {
-            dispatch(addCategory(selectedCategory))
-            dispatch(addQuantity(quantity))
-            dispatch(addImage(image))
+            const { selectedCategory, checkedMass, quantity, image } = productDetail
+            dispatch(setProduct({
+                category: selectedCategory,
+                quantity: quantity,
+                mass: checkedMass,
+                image: image,
+            }))
             navigation.goBack()
         }
     }
@@ -99,7 +107,7 @@ const OrderDetail = ({ navigation }) => {
                     </View>
                     {categories.map(ele => (
                         <TouchableOpacity
-                            onPress={() => setSelectedCategory(cateId => cateId === ele.id ? null : ele.id)}
+                            onPress={() => updateProductDetail({ selectedCategory: productDetail.selectedCategory === ele.id ? null : ele.id })}
                             key={ele.id}
                             className="flex-row justify-between items-center pl-10"
                         >
@@ -107,9 +115,9 @@ const OrderDetail = ({ navigation }) => {
                             <View>
                                 <CheckBox
                                     style={{ flex: 1, padding: 10 }}
-                                    onClick={() => setSelectedCategory(cateId => cateId === ele.id ? null : ele.id)}
+                                    onClick={() => updateProductDetail({ selectedCategory: productDetail.selectedCategory === ele.id ? null : ele.id })}
                                     checkedCheckBoxColor='#3422F1'
-                                    isChecked={selectedCategory === ele.id}
+                                    isChecked={productDetail.selectedCategory === ele.id}
                                 />
                             </View>
                         </TouchableOpacity>
@@ -128,8 +136,8 @@ const OrderDetail = ({ navigation }) => {
                         className="text-xl font-semibold px-3 py-[-12]"
                         keyboardType='numeric'
                         defaultValue='1'
-                        value={String(quantity)}
-                        onChangeText={txt => setQuantity(txt)}
+                        value={String(productDetail.quantity)}
+                        onChangeText={txt => updateProductDetail({ quantity: txt })}
                         ref={inputRef}
                     />
                 </TouchableOpacity>
@@ -148,12 +156,14 @@ const OrderDetail = ({ navigation }) => {
                                 key={c.item.id}
                                 underlayColor={'white'}
                                 className='rounded-3xl py-3 px-8 border border-gray-600 ml-5'
-                                style={checkedMass === c.item.id && { borderBlockColor: "#3422F1", backgroundColor: 'rgba(134, 182, 246, 0.4)' }}
-                                onPress={() => setCheckedMass(checkedMass => checkedMass === c.item.id ? null : c.item.id)}
+                                style={productDetail.checkedMass === c.item.id && { borderBlockColor: "#3422F1", backgroundColor: 'rgba(134, 182, 246, 0.4)' }}
+                                onPress={() => updateProductDetail({
+                                    checkedMass: productDetail.checkedMass === c.item.id ? null : c.item.id
+                                })}
                             >
                                 <Text
                                     className="text-black font-medium"
-                                    style={checkedMass === c.item.id && { color: "#3422F1" }}
+                                    style={productDetail.checkedMass === c.item.id && { color: "#3422F1" }}
                                 >
                                     {c.item.name}
                                 </Text>
@@ -170,9 +180,9 @@ const OrderDetail = ({ navigation }) => {
                     <TouchableOpacity
                         onPress={pickImage}
                         className="flex-col justify-center space-y-2 items-center border border-gray-600 rounded-xl w-24 h-24 ml-10"
-                        style={image && { backgroundColor: 'rgb(147, 197, 253)' }}
+                        style={productDetail.image && { backgroundColor: 'rgb(147, 197, 253)' }}
                     >
-                        {image ? (
+                        {productDetail.image ? (
                             <>
                                 <AntDesign name="checkcircle" size={24} color="#3422F1" />
                                 <Text className="font-medium text-[#3422F1]">Đã tải ảnh</Text>
